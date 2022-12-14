39ab8b7b9bb05e15a85fa55e475008c152923319
anaskhan28 <khananasatique@gmail.com>
2022-12-14 22:39:18 +0530

    feature: added macos style navbar

--- chat-app/src/pages/Home.jsx
+++ chat-app/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Chat from '../components/Chat';
-
+import Menu from '../images/MenuBar.png';
 function Home() {
   return (
+    <>
+    <img className='menu' src={Menu} alt="menu"/>
     <div className='home'>
         <div className="container">
             <Sidebar/>
             <Chat/>
         </div>
     </div>
+    </>
   )
 }
 
