ba6dc2e405416cf27f68833e59bb2e29b6c39859
anaskhan28 <khananasatique@gmail.com>
2022-12-15 10:33:07 +0530

    bug: message ui fixed

--- chat-app/src/firebase.js
+++ chat-app/src/firebase.js
@@ -14,8 +14,45 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID 
 };
 
+
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db =  getFirestore();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAlpXJagVJfM7xmYAg5OysiCpcP64JqAm4",
+//   authDomain: "scrappy-chat.firebaseapp.com",
+//   projectId: "scrappy-chat",
+//   storageBucket: "scrappy-chat.appspot.com",
+//   messagingSenderId: "1069247591006",
+//   appId: "1:1069247591006:web:b8bdf70beef40c7457bacf",
+//   measurementId: "G-9T7C60TYPZ" 
+// };
